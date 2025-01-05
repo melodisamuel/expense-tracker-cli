3edbb3acb1136a8942140e7b1edd972187402648
Melody <melodysamuelcodes@gmail.com>
2025-01-05 17:48:23 +0100

    # Write expenses function and read expenses function

---
+++ expense-tracker-cli/src/utils.ts
@@ -0,0 +1,17 @@
+import fs from 'fs';
+
+import { Expense } from './expenses';
+
+const DATA_FILE = './expenses.json';
+
+export const readExpenses = (): Expense[] => {
+    if(!fs.existsSync(DATA_FILE)) {
+        return [];
+    }
+    const data = fs.readFileSync(DATA_FILE, 'utf-8');
+    return JSON.parse(data);
+}
+
+export const writeExpenses = (expenses: Expense[]): void => {
+    fs.writeFileSync(DATA_FILE, JSON.stringify(expenses, null, 2));
+}
